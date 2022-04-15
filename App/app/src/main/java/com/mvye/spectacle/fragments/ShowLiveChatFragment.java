06cd2665d925c4018af96d1689acc46e737769c4
Karel Rojas <karelrojas64@gmail.com>
2022-04-14 22:04:01 -0400

    Updated live chat and search show fragments

--- App/app/src/main/java/com/mvye/spectacle/fragments/ShowLiveChatFragment.java
+++ App/app/src/main/java/com/mvye/spectacle/fragments/ShowLiveChatFragment.java
@@ -2,15 +2,24 @@ package com.mvye.spectacle.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.mvye.spectacle.R;
 
 public class ShowLiveChatFragment extends Fragment {
+
+    private ImageView ivLivechatPoster;
+    private TextView tvTitleChat;
+    private RecyclerView rvComments;
 
     public ShowLiveChatFragment() {
         // Required empty public constructor
@@ -21,5 +30,12 @@ public class ShowLiveChatFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_show_live_chat, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
--- App/app/src/main/java/com/mvye/spectacle/fragments/ShowSearchFragment.java
+++ App/app/src/main/java/com/mvye/spectacle/fragments/ShowSearchFragment.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.mvye.spectacle.R;
 
 public class ShowSearchFragment extends Fragment {
+
+    private TextView tvShowSearch;
+    private RecyclerView rvShowList;
 
     public ShowSearchFragment() {
         // Required empty public constructor
@@ -28,7 +33,6 @@ public class ShowSearchFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
 
     }
 }
