98a1c797871944b417f85f8fa4e52fe1344739e5
cdd24 <cdd24@njit.edu>
2022-04-27 12:19:24 -0400

    Carlos and Mervyn merge

--- App/app/src/main/java/com/mvye/spectacle/LoginActivity.java
+++ App/app/src/main/java/com/mvye/spectacle/LoginActivity.java
@@ -19,25 +19,30 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnReturn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        if(ParseUser.getCurrentUser() != null) {
-            goMainActivity();
-        }
+        etUsername = findViewById(R.id.etLoginUsername);
+        etPassword = findViewById(R.id.etLoginPassword);
+        btnLogin   = findViewById(R.id.btnStartupLogin);
+        btnReturn  = findViewById(R.id.btnLoginReturn);
 
-        etUsername = findViewById(R.id.etUsername);
-        etPassword = findViewById(R.id.etPassword);
-        btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 loginUser(username, password);
+            }
+        });
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goStartupActivity();
             }
         });
     }
@@ -56,6 +61,12 @@ public class LoginActivity extends AppCompatActivity {
                 Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void goStartupActivity() {
+        Intent i = new Intent(this, StartupActivity.class);
+        startActivity(i);
+        finish();
     }
 
     private void goMainActivity() {
